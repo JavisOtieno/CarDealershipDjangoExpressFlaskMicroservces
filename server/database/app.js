56c109993183572f5da2233aedcd69ba1e4b9fad
javisotieno <javisotieno@gmail.com>
2025-02-18 20:07:20 -0500

    dealer mongoose express endpoints

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,46 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+try {
+  const documents = await Dealerships.find({});
+  res.json(documents);
+} catch (error) {
+  res.status(500).json({ error: 'Error fetching documents' });
+}
+
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+  try {
+    const dealers = await Dealerships.find({ state: req.params.state });
+    res.json(dealers);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+// res.status(500).json({ error: 'Error Test' });
+
+try {
+    // Convert the route parameter to a number and search using the 'id' field from the schema
+    const dealer = await Dealerships.findOne({ id: Number(req.params.id) });
+    
+    if (!dealer) {
+      return res.status(404).json({ error: 'Dealer not found' });
+    }
+    
+    res.json(dealer);
+  } catch (error) {
+    // res.status(500).json({ error: 'Error fetching dealership' });
+    res.status(500).send(error.stack); // Sends the full error stack for debugging
+
+  }
+
 });
 
 //Express route to insert review
